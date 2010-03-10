4504b5bae13232ecf9572c53c3b0051bc6964267
Ali Yahya <git@ali01.com>
2010-03-10 11:34:29 -0800

    laid out inheritance hierarchy

--- src/radon.cpp
+++ src/radon.cpp
@@ -7,21 +7,30 @@ using std::string;
 
 #include "dataset-parser.h"
 #include "dataset-description.h"
+#include "lr-classifier.h"
+#include "nb-classifier.h"
+#include "prediction.h"
 using Radon::DatasetParser;
 using Radon::DatasetDescription;
+using Radon::LRClassifier;
+using Radon::NBClassifier;
+using Radon::Prediction;
 
 int main() {
+  string filepath_prefix, filepath_suffix;
   DatasetDescription::Ptr train_dataset, test_dataset;
-  DatasetParser::Ptr data_parser = DatasetParser::DatasetParserNew();
+  DatasetParser::Ptr data_parser;
+  NBClassifier::Ptr nb_classifier;
+  LRClassifier::Ptr lr_classifier;
+  Prediction::Ptr nb_mle_prediction, nb_laplace_prediction, lr_prediction;
 
-  string filepath_prefix;
-  filepath_prefix += Radon::kDatasetPath;
+  filepath_prefix =  Radon::kDatasetPath;
   filepath_prefix += "/";
   filepath_prefix += Radon::kDataset;
 
-  string filepath_suffix;
-  filepath_suffix += Radon::kExtension;
+  filepath_suffix = Radon::kExtension;
 
+  data_parser = DatasetParser::DatasetParserNew();
   train_dataset = data_parser->datasetDescription(filepath_prefix +
                                                   Radon::kTrainFlag +
                                                   filepath_suffix);
@@ -29,4 +38,17 @@ int main() {
   test_dataset = data_parser->datasetDescription(filepath_prefix +
                                                  Radon::kTestFlag +
                                                  filepath_suffix);
+
+  nb_classifier = NBClassifier::NBClassifierNew();
+  nb_classifier->trainDatasetIs(train_dataset);
+  nb_classifier->testDatasetIs(test_dataset);
+
+  nb_mle_prediction = nb_classifier->prediction(NBClassifier::kML);
+  nb_laplace_prediction = nb_classifier->prediction(NBClassifier::kLaplace);
+
+  lr_classifier = LRClassifier::LRClassifierNew();
+  lr_classifier->trainDatasetIs(train_dataset);
+  lr_classifier->testDatasetIs(test_dataset);
+
+  lr_prediction = lr_classifier->prediction();
 }
