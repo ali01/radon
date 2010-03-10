9eed2b5e556a5ff11d0f5627bf3a54dac35fd895
Ali Yahya <git@ali01.com>
2010-03-10 05:36:28 -0800

    added prediction class, began fleshing out naive-bayes implementation

--- src/naive-bayes.cpp
+++ src/naive-bayes.cpp
@@ -2,6 +2,13 @@
 
 namespace Radon {
 
+NaiveBayesClassifier::NaiveBayesClassifier(DatasetDescription::Ptr _train_data,
+                                           DatasetDescription::Ptr _test_data)
+  : train_data_(_train_data), test_data_(_test_data) {}
 
+Prediction::Ptr
+NaiveBayesClassifier::prediction(EstimationMode _mode) {
+  
+}
 
 } /* end of namespace Radon */
--- src/naive-bayes.h
+++ src/naive-bayes.h
@@ -3,18 +3,33 @@
 
 #include <simone/ptr_interface.h>
 
+#include "dataset-description.h"
+#include "prediction.h"
+
 namespace Radon {
 
 class NaiveBayesClassifier : public Simone::PtrInterface<NaiveBayesClassifier> {
 public:
   typedef Simone::Ptr<const NaiveBayesClassifier> PtrConst;
   typedef Simone::Ptr<NaiveBayesClassifier> Ptr;
-  
-  static Ptr NaiveBayesClassifierNew() { return new NaiveBayesClassifier(); }
+
+  enum EstimationMode { kMLEstimation, kLaplaceEstimation };
+
+  static Ptr NaiveBayesClassifierNew(DatasetDescription::Ptr _train_data,
+                                     DatasetDescription::Ptr _test_data) {
+    return new NaiveBayesClassifier(_train_data, _test_data);
+  }
+
+  Prediction::Ptr prediction(EstimationMode _mode);
 
 private:
-  NaiveBayesClassifier() {}
-  
+  NaiveBayesClassifier(DatasetDescription::Ptr _train_data,
+                       DatasetDescription::Ptr _test_data);
+
+  /* data members */
+  DatasetDescription::Ptr train_data_;
+  DatasetDescription::Ptr test_data_;
+
   /* disallowed operations */
   NaiveBayesClassifier(const NaiveBayesClassifier&);
   NaiveBayesClassifier& operator=(const NaiveBayesClassifier&);
