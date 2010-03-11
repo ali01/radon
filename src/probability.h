7b5905756ff2e731976598ac96afd9d9c708a828
Ali Yahya <git@ali01.com>
2010-03-10 17:36:06 -0800

    added probability

--- src/probability.h
+++ src/probability.h
@@ -5,10 +5,11 @@
 
 namespace Radon {
 
-// class Probability : public Simone::Numeric<Probability,double> {
-// public:
-//
-// };
+class Probability : public Simone::Numeric<Probability,double> {
+public:
+  Probability() : Simone::Numeric<Probability,double>(1.0) {}
+  Probability(double v) : Simone::Numeric<Probability,double(v) {}
+};
 
 } /* end of namespace radon */
 
